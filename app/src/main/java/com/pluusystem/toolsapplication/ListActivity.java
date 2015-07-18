0b4c102a82c9cc827e0045d4e4d5eff150307390
Pluu <pluulove@gmail.com>
2015-07-18 12:48:17 +0900

    Update

--- app/src/main/java/com/pluusystem/toolsapplication/ListActivity.java
+++ app/src/main/java/com/pluusystem/toolsapplication/ListActivity.java
@@ -2,9 +2,14 @@ package com.pluusystem.toolsapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +22,9 @@ public class ListActivity extends AppCompatActivity {
 
 		ListView listView = (ListView) findViewById(R.id.listView);
 
-		List<String> list = new ArrayList<>();
-		for (int i = 0; i < 20; i++) {
+		final int size = 20;
+		List<String> list = new ArrayList<>(size);
+		for (int i = 0; i < size; i++) {
 			list.add("Item " + i);
 		}
 		ArrayAdapter<String> adapter
@@ -26,4 +32,43 @@ public class ListActivity extends AppCompatActivity {
 		listView.setAdapter(adapter);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_list, menu);
+		return true;
+	}
+
+	@Override
+	protected boolean onPrepareOptionsPanel(View view, Menu menu) {
+		if (menu != null) {
+			if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+				try {
+					Method m = menu.getClass().getDeclaredMethod(
+						"setOptionalIconsVisible", Boolean.TYPE);
+					m.setAccessible(true);
+					m.invoke(menu, true);
+				} catch (Exception e) {
+					Log.e(getClass().getSimpleName(),
+						  "onMenuOpened...unable to set icons for overflow menu", e);
+				}
+			}
+		}
+		return super.onPrepareOptionsPanel(view, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
